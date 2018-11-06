c62b7db8e291540a5b2ff1df7bebf7414df6a1c0
Edgars Zabroda <28626763+EdgarsZabroda@users.noreply.github.com>
2018-11-06 05:29:49 +0200

    Arranging items
    In this lesson I've learned how to arrange the items, with this help, I'm soon am ready to release a Source Cipher, it will be added to the another repository, stay tuned ;)

---
+++ src/aranging/items/ArangingItems.java
@@ -0,0 +1,81 @@
+package aranging.items;
+
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+
+public class ArangingItems extends JFrame
+{
+	private static final long serialVersionUID = 0x7FEEDDCCL;
+	
+	public ArangingItems()
+	{
+		setTitle("Aranging items");
+		setSize(256, 256);
+		setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JPanel jPanel = new JPanel();
+		jPanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		setVisible(true);
+		
+		JLabel jlOne = new JLabel("This");
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2;
+		gbc.gridheight = 1;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(8, 10, 0, 0);
+		jPanel.add(jlOne, gbc);
+		JLabel jlTwo = new JLabel("is");
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 2;
+		gbc.gridheight = 1;
+		gbc.anchor = GridBagConstraints.WEST;
+		jPanel.add(jlTwo, gbc);
+		JLabel jlThree = new JLabel("manually");
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		jPanel.add(jlThree, gbc);
+		JLabel jlFour = new JLabel("aligned");
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		jPanel.add(jlFour, gbc);
+		
+		jPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		getContentPane().add(jPanel, BorderLayout.NORTH);
+	}
+	
+	public static void gui()
+	{
+		try
+		{
+			UIManager.setLookAndFeel("com.sun.java.swing.pluf.nymph.NympLookAndFeel");
+		}
+		catch(Exception E)
+		{
+			System.out.println(E.toString());
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] Args)
+	{
+		ArangingItems ai = new ArangingItems();
+	}
+}
